be5dfbea3a4da745eeeb05be6e39c0483c0344f0
Tom Wilkins <tom.wilkins@citrix.com>
2017-12-22 13:44:33 +0000

    Created basic Windows Service following this guide: https://docs.microsoft.com/en-us/dotnet/framework/windows-services/walkthrough-creating-a-windows-service-application-in-the-component-designer

---
+++ Services/MyNewService/MyNewService/MyNewService.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Runtime.InteropServices;
+
+namespace MyNewService
+{
+    public partial class MyNewService : ServiceBase
+    {
+
+        private int eventId = 1;
+
+        public MyNewService(string[] args)
+        {
+            InitializeComponent();
+            string eventSourceName = "MySource";
+            string logName = "MyNewLog";
+            if(args.Count() > 0)
+            {
+                eventSourceName = args[0];
+            }
+            if(args.Count() > 1)
+            {
+                logName = args[1];
+            }
+
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists(eventSourceName))
+            {
+                EventLog.CreateEventSource(eventSourceName, logName);
+            }
+            eventLog1.Source = eventSourceName;
+            eventLog1.Log = logName;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+
+            // Update service state to pending
+            ServiceStatus status = new ServiceStatus();
+            status.dwCurrentState = ServiceState.SERVICE_START_PENDING;
+            status.dwWaitHint = 100000;
+            SetServiceStatus(this.ServiceHandle, ref status);
+
+            eventLog1.WriteEntry("In OnStart");
+
+            // Sets an event to occur every minute
+            //TODO wrap this in a background thread.
+            Timer timer = new Timer();
+            timer.Interval = 60000;
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+
+            status.dwCurrentState = ServiceState.SERVICE_RUNNING;
+            SetServiceStatus(this.ServiceHandle, ref status);
+
+        }
+
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
+            
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In OnStop");
+        }
+
+        protected override void OnContinue()
+        {
+            eventLog1.WriteEntry("In OnContinue");
+        }
+
+        public enum ServiceState
+        {
+            SERVICE_STOPPED = 0x00000001,
+            SERVICE_START_PENDING = 0x00000002,
+            SERVICE_STOP_PENDING = 0x00000003,
+            SERVICE_RUNNING = 0x00000004,
+            SERVICE_CONTINUE_PENDING = 0x00000005,
+            SERVICE_PAUSE_PENDING = 0x00000006,
+            SERVICE_PAUSED = 0x00000007,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ServiceStatus
+        {
+            public int dwServiceType;
+            public ServiceState dwCurrentState;
+            public int dwControlsAccepted;
+            public int dwWin32ExitCode;
+            public int dwServiceSpecificExitCode;
+            public int dwCheckPoint;
+            public int dwWaitHint;
+        };
+
+        [DllImport("advapi32.dll", SetLastError = true)]
+        private static extern bool SetServiceStatus(IntPtr handle, ref ServiceStatus serviceStatus);
+
+
+    }
+}
